6dcf7bcf729df572a4489c8f58288275088acd92
Mariya-Lutsenko <107764269+Mariya-Lutsenko@users.noreply.github.com>
2023-02-04 21:07:20 +0200

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import  { useState, useEffect } from 'react';
 import css from './App.module.css';
 import { nanoid } from 'nanoid';
 import ContactForm from './ContactForm';
